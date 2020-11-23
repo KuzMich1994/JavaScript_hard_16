6dbcaceb7c19834f71e9df2cb8324a0330dbaeb5
KuzMich1994 <drumer19940310@mail.ru>
2020-11-23 18:40:28 +0300

    Создал отдельную ветку под урок 3

--- js/main.js
+++ js/main.js
@@ -1,22 +1,2 @@
 'use strict';
-let num = 266219;
 
-let strNum = String(num);
-let result = 1;
-
-for (let i = 0; i < strNum.length; i++) {
-  result *= strNum[i];
-}
-
-result = result ** 3;
-
-let resultStr = String(result);
-console.log(resultStr.substring(0, 2));
-
-
-let num2 = 266219;
-
-console.log((num2.toString().split('').reduce( (acc, e) => acc * e) ** 3).toString().substring(0, 2));
-
-// num2 = num2 ** 3;
-// console.log(num2.toString());
