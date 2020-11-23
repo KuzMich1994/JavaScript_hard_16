82abc505c8ab33ac42d418ae92c101c98ec060c6
KuzMich1994 <drumer19940310@mail.ru>
2020-11-23 18:36:52 +0300

    Добавил решение через reduce()

--- js/main.js
+++ js/main.js
@@ -12,3 +12,11 @@ result = result ** 3;
 
 let resultStr = String(result);
 console.log(resultStr.substring(0, 2));
+
+
+let num2 = 266219;
+
+console.log((num2.toString().split('').reduce( (acc, e) => acc * e) ** 3).toString().substring(0, 2));
+
+// num2 = num2 ** 3;
+// console.log(num2.toString());
