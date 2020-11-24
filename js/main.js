ed117f7820d821bed9e4ea1611522bfe6e22055e
KuzMich1994 <drumer19940310@mail.ru>
2020-11-25 00:27:09 +0300

    Изменил конструкцию

--- js/main.js
+++ js/main.js
@@ -1,14 +1,16 @@
 'use strict';
 let firstVariable = prompt('Введите данные');
-let secondVariable = 20;
+const secondVariable = 20;
 
 function oneArg(arg) {
-  if (typeof arg === 'string' && arg.length > 30) {
-    return arg.trim().slice(0, 30) + '...';
-  } else if (typeof arg === 'string') {
-    return arg.trim();
-  } else {
+  if (typeof arg !== 'string') {
     return 'Это не строка';
+  } else {
+    if (arg.length > 30)  {
+      arg = arg.trim().slice(0, 30) + '...';
+      return arg;
+    }
+    return arg.trim();
   }
 }
 
