e9d9aaf6cb31f154182d37baa57d7950e6b2c4ae
KuzMich1994 <drumer19940310@mail.ru>
2020-11-24 23:39:16 +0300

    2000 years later  выполнил усложненное задание к уроку №4

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
 'use strict';
+let firstVariable = prompt('Введите данные');
+let secondVariable = 20;
+
+function oneArg(arg) {
+  if (typeof arg === 'string' && arg.length > 30) {
+    return arg.trim().slice(0, 30) + '...';
+  } else if (typeof arg === 'string') {
+    return arg.trim();
+  } else {
+    return 'Это не строка';
+  }
+}
+
+console.log(oneArg(firstVariable));
+console.log(oneArg(secondVariable));
+
+let a = 'ну как тебя сука сделать';
+console.log(a.slice(0, 11) + '...');
