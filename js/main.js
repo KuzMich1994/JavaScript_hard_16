0e51169cc590fd63fb3416f5bd681f796146b90f
KuzMich1994 <drumer19940310@mail.ru>
2020-11-27 00:06:40 +0300

    Выполнил усложненное задание к уроку №5

--- js/main.js
+++ js/main.js
@@ -1,18 +1,25 @@
 'use strict';
-let firstVariable = prompt('Введите данные');
-const secondVariable = 20;
+// Задание 1 (1 способ)
+let arr = ['12345', '22345', '32345', '42345', '52345', '62345', '21345'];
+console.log(arr);
 
-function oneArg(arg) {
-  if (typeof arg !== 'string') {
-    return 'Это не строка';
-  } else {
-    if (arg.length > 30)  {
-      arg = arg.trim().slice(0, 30) + '...';
-      return arg;
-    }
-    return arg.trim();
+const newArr = arr.map(function(item) {
+  if (item.startsWith('2') || item.startsWith('4')) {
+    console.log(item);
   }
-}
+  return item;
+});
 
-console.log(oneArg(firstVariable));
-console.log(oneArg(secondVariable));
+console.log('');
+// Задание 1 (Второй способ)
+let arr2 = ['12345', '22345', '32345', '42345', '52345', '62345', '21345'];
+console.log(arr2);
+
+arr2.forEach(function(item) {
+  if (item.startsWith('2') || item.startsWith('4')) {
+    console.log(item);
+  }
+});
+
+// Задание 2
+
