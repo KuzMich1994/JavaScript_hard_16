f5eb208e4e8c02fa4fe929107820f2382fb65ca5
KuzMich1994 <drumer19940310@mail.ru>
2020-11-30 01:08:20 +0300

    Выполнил усложненное задание урока №7

--- js/main.js
+++ js/main.js
@@ -1,3 +1,33 @@
 'use strict';
+let week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
+today = new Date(),
+weekDay = today.getDay(),
+options = {weekday: 'long'},
+weekday = new Intl.DateTimeFormat('ru-RU', options).format(today);
+    console.log(weekday);
+
+
+let outWeek = document.querySelector('.text');
+let str = ' ';
+
+for (let i = 0; i < week.length; i++) {
+  if (i === (weekDay - 1) || i === (weekDay + 6)) {
+    str += week[i].bold() + '<br>';
+  } else if (week[i] === 'Суббота' || week[i] === 'Воскресенье') {
+    str += week[i].italics() + '<br>';
+  } else {
+    str += week[i] + '<br>';
+  }
+}
+
+outWeek.innerHTML = str;
+
+
+
+
+
+
+
+
 
 
