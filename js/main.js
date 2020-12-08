d97ae7a072ebfaa7c0876820073a8c232415f34f
KuzMich1994 <drumer19940310@mail.ru>
2020-12-08 19:42:27 +0300

    Выполнил усложненное задание урока №13

--- js/main.js
+++ js/main.js
@@ -1,54 +1,13 @@
 'use strict';
-let today = new Date(),
-now = today.getDay(),
-nowMinutes = today.getMinutes(),
-nowSeconds = today.getSeconds(),
-nowHours = today.getHours(),
-options = {
-  weekday: 'long',
-  day: 'numeric',
-  month: 'long',
-  year: 'numeric',
-  hour: 'numeric',
-  minute: 'numeric'
-},
-weekday = new Intl.DateTimeFormat('ru-Ru', options).format(today);
+const randomButton = document.querySelector('.random-color'),
+canvas = document.querySelector('.canvas'),
+randomTextColor = document.querySelector('.random-text-color'),
+getRandom = function() {
+  let randomColor = '#' + (Math.random().toString(16) + '000000').substring(2, 8).toUpperCase();
+  randomColor = canvas.style.backgroundColor = `${randomColor}`;
+  randomColor = randomTextColor.textContent = randomColor;
+};
 
-let outDay = document.querySelector('.text');
-outDay.style.color = 'red';
-let str = `Сегодня ${weekday} ,`;
-
-outDay.innerHTML = str;
-
-// let week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
-// today = new Date(),
-// weekDay = today.getDay(),
-// options = {weekday: 'long'},
-// weekday = new Intl.DateTimeFormat('ru-RU', options).format(today);
-//     console.log(weekday);
-
-
-// let outWeek = document.querySelector('.text');
-// let str = ' ';
-
-// for (let i = 0; i < week.length; i++) {
-//   if (i === (weekDay - 1) || i === (weekDay + 6)) {
-//     str += week[i].bold() + '<br>';
-//   } else if (week[i] === 'Суббота' || week[i] === 'Воскресенье') {
-//     str += week[i].italics() + '<br>';
-//   } else {
-//     str += week[i] + '<br>';
-//   }
-// }
-
-// outWeek.innerHTML = str;
-
-
-
-
-
-
-
-
-
-
+randomButton.addEventListener('click', function() {
+  getRandom();
+});
