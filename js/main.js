6ad5bf6ee6a25db46955ff66b5dcabe96e787101
KuzMich1994 <drumer19940310@mail.ru>
2020-12-08 20:06:10 +0300

    И еще

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ canvas = document.querySelector('.canvas'),
 randomTextColor = document.querySelector('.random-text-color'),
 getRandom = function() {
   let randomColor = '#' + (Math.random().toString(16)).substring(2, 8).toUpperCase();
-  canvas.style.backgroundColor = `${randomColor}`;
+  canvas.style.backgroundColor = randomColor;
   randomTextColor.textContent = randomColor;
 };
 
